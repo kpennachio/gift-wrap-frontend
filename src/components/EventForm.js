b6165a5f4da5ef33c7088594d1c50ce86cf12d88
cannoli9917 <katie.pennachio@gmail.com>
2019-04-03 22:18:31 -0400

    event form with semantic

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import 'react-day-picker/lib/style.css';
+import { Button, Checkbox, Form, Input, Dropdown, Select, TextArea } from 'semantic-ui-react'
+
 
 
 export default class EventForm extends Component {
@@ -10,7 +12,9 @@ export default class EventForm extends Component {
     date: "",
     notes: "",
     registry_link: "",
-    person1: ""
+    people: [],
+    showingNewPersonInput: false,
+    newPersonName: ""
   }
 
   handleChange = (e) => {
@@ -21,35 +25,74 @@ export default class EventForm extends Component {
     this.setState({day})
   }
 
+  handleDropdown = (e) => {
+    console.log(e.target.value);
+  }
+
+  dropdownOptions = () => {
+    const people = [{id:1, name:"allie"}, {id:2, name:"mom"}]
+    return people.map(person => {
+      return {key: person.id, text: person.name, value: person.id}
+    })
+  }
+
+  showInput = (e) => {
+    this.setState({showingNewPersonInput: true})
+    e.target.innerText = ""
+  }
+
+
+
   render() {
     return(
       <div>
         <h2>Add Event</h2>
+          <Form>
 
-          <form onSubmit={console.log("submit")}>
-            <label>
-              Event:
-              <input type="text" name="event" value={this.state.event} onChange={this.handleChange} />
-            </label>
-            <label>
-              Date:
-              <DayPickerInput onDayChange={this.handleDayChange}/>
-            </label>
-            <label>
-              Notes:
-              <input type="text" name="notes" placeholder="Optional" value={this.state.notes} onChange={this.handleChange} />
-            </label>
-            <label>
-              Registry Link:
-              <input type="text" name="registry_link" placeholder="Optional" value={this.state.registry_link} onChange={this.handleChange} />
-            </label>
-            <label>
-              Person:
-              <input type="text" name="person" onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Add Event" />
-          </form>
+              <Form.Field control={Input} label='Event' placeholder='Event' onChange={this.handleChange}/>
+
+              <Form.Field label='Date' control={DayPickerInput} onDayChange={this.handleDayChange}/>
+
+              <Form.Field control={Input} label='Who gets a gift?'
+                control={Dropdown}
+                placeholder='Choose Person...'
+                fluid
+                multiple
+                search
+                selection
+                options={this.dropdownOptions()}
+                onChange={this.handleDropdown}
+              />
+              <Form.Group
+                style={{ display: (this.state.showingNewPersonInput ? 'block' : 'none') }}>
+              <Form.Field control={Input} label='Add a New Person' placeholder='Name' />
+              </Form.Group>
+
+              <p onClick={this.showInput}>Add a new person?</p><br/>
+
+
+              <Form.Field control={Input} label='Registry Link' placeholder='Registry Link (optional)' onChange={this.handleChange}/>
+
+
+              <Form.Field control={TextArea} label='Notes' placeholder='Notes
+              (optional)' onChange={this.handleChange}/>
+
+            <Form.Field control={Button}>Add Event</Form.Field>
+          </Form>
       </div>
     )
   }
 }
+
+// datalist
+// list='people' placeholder='Choose Person...' />
+//   <datalist id='people'>
+//     {this.datalistOptions()}
+//   </datalist>
+
+// datalistOptions = () => {
+//   const people = ["allie", "mom", "dad", "kevin"]
+//   return people.map(person => {
+//     return <option value={person} />
+//   })
+// }
