82a184663ecb1e303f569766bfef1f4e5235207b
cannoli9917 <katie.pennachio@gmail.com>
2019-04-06 19:39:19 -0400

    add person form

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -16,13 +16,46 @@ class PersonForm extends Component {
     this.setState({[e.target.name]: e.target.value})
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.createNewPerson()
+    this.setState({
+      name: "",
+      notes: ""
+    })
+  }
+
+  createNewPerson = () => {
+    let data = {
+      user_id: this.props.currentUser.id,
+      name: this.state.name,
+      notes: this.state.notes
+    }
+    fetch('http://localhost:3000/api/v1/people', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then(resp => resp.json())
+    .then(person => {
+      console.log(person);
+      person.person_gift_events = []
+      person.person_gift_ideas = []
+      this.props.addNewPerson(person)
+    })
+  }
+
   render() {
     return(
       <div>
         <h2>Add a new person</h2>
-        <Form>
-          <Form.Field control={Input} name="name" label='Name' placeholder='Name' onChange={this.handleChange} />
-          <Form.Field control={Input} name="notes" label='Notes' placeholder='Notes' onChange={this.handleChange} />
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field control={Input} value={this.state.name} name="name" label='Name' placeholder='Name' onChange={this.handleChange} />
+          <Form.Field control={Input} value={this.state.notes} name="notes" label='Notes' placeholder='Notes' onChange={this.handleChange} />
+          <Button type='submit'>Add Person</Button>
         </Form>
       </div>
     )
@@ -32,14 +65,13 @@ class PersonForm extends Component {
 
 function mapStateToProps(state) {
   return {
-
+    currentUser: state.currentUser
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return  {
-
+    addNewPerson: (person) => dispatch({type: "ADD_NEW_PERSON", payload: person}),
   }
 }
-
 export default connect(mapStateToProps, mapDispatchToProps)(PersonForm);
