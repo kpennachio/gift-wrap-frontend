006f133e83ae8e59748f6ad6778490317a24f55c
cannoli9917 <katie.pennachio@gmail.com>
2019-04-08 14:53:55 -0400

    add circular progress bar

--- src/components/budget_page/BudgetContainer.js
+++ src/components/budget_page/BudgetContainer.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux'
 
-
+import CircularProgressbar from 'react-circular-progressbar';
 
 
 const BudgetContainer = (props) => {
 
+  const percentage = 80
   return (
     <div>
       <h1>Budget Container</h1>
+      <div className="budget-main-circle">
+        <CircularProgressbar
+          percentage={percentage}
+          text={`${percentage}%`}
+        />
+      </div>
     </div>
   );
 
