18876ef869444bcfed64f5f2702169da26066cc2
cannoli9917 <katie.pennachio@gmail.com>
2019-04-12 10:05:41 -0400

    events separated by month

--- src/components/EventContainer.js
+++ src/components/EventContainer.js
@@ -27,12 +27,55 @@ const EventContainer = (props) => {
     }
   }
 
+  const eventsByMonth = () => {
+    let ordered = {}
+    orderEvents().forEach(event => {
+      if (!ordered[event.year]) {
+        ordered[event.year] = {}
+        ordered[event.year][event.month] = []
+        ordered[event.year][event.month].push(event)
+      }
+      else if (!ordered[event.year][event.month]) {
+        ordered[event.year][event.month] = []
+        ordered[event.year][event.month].push(event)
+      }
+      else {
+        ordered[event.year][event.month].push(event)
+      }
+    })
+    return ordered
+  }
+
+  const renderEvents = () => {
+    let yearObj = eventsByMonth()
+    let array = []
+    for (const year in yearObj) {
+      for (const month in yearObj[year]) {
+        array.push(month)
+        for (const event in yearObj[year][month]) {
+          array.push(yearObj[year][month][event])
+        }
+      }
+    }
+    console.log(array);
+    return array.map(a => {
+      if (typeof a === "string"){
+        return <p>a</p>
+      }
+      else {
+        return <Event key={uuid()} event={a} pge={a.person_gift_events}/>
+      }
+    })
+  }
+
+
+
   return (
     <div >
 
       <EventForm />
       <h2>all events</h2>
-      {renderAllEvents()}
+      {renderEvents()}
     </div>
   );
 
