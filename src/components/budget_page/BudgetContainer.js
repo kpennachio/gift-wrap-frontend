5d2c72aeebb885217e0fe8a337ee217c01654de1
cannoli9917 <katie.pennachio@gmail.com>
2019-04-30 09:54:22 -0400

    comments on budget container

--- src/components/budget_page/BudgetContainer.js
+++ src/components/budget_page/BudgetContainer.js
@@ -12,6 +12,8 @@ import { Icon } from 'semantic-ui-react'
 
 const BudgetContainer = ({events, year, changeYear, budget, budgets, setBudget}) => {
 
+
+  // add all budgets for each person in each event for given year to get total allocated budget
   const allocatedBudget = () => {
     let selectedEvents = events.filter(event => event.year === year)
       let subtotal = selectedEvents.map(event => {
@@ -29,6 +31,7 @@ const BudgetContainer = ({events, year, changeYear, budget, budgets, setBudget})
       else return 0
   }
 
+  // add all gift costs for each person in each event for given year to get total amount paid
   const amountPaid = () => {
     let selectedEvents = events.filter(event => event.year === year)
       let subtotal = selectedEvents.map(event => {
@@ -47,6 +50,7 @@ const BudgetContainer = ({events, year, changeYear, budget, budgets, setBudget})
   }
 
 
+  // callbacks for arrows on click - change year back or forward
   const yearBack = () => {
     changeYear(--year)
     changeBudget()
@@ -57,6 +61,7 @@ const BudgetContainer = ({events, year, changeYear, budget, budgets, setBudget})
     changeBudget()
   }
 
+  // on year change, find budget for that year and change in state
   const changeBudget = () => {
     let budgetObj = budgets.find(budget => budget.year === year)
     if (budgetObj) {
@@ -68,8 +73,11 @@ const BudgetContainer = ({events, year, changeYear, budget, budgets, setBudget})
     }
   }
 
+  // percentage of gift budget spent
+  // if nothing has been budgeted, return 0%
+  // else divide (amount spent)/(allocated budget) * 100 for percent
   const paidPercentage = () => {
-    if (amountPaid() === 0) {
+    if (allocatedBudget() === 0) {
       return 0
     }
     else {
@@ -77,6 +85,9 @@ const BudgetContainer = ({events, year, changeYear, budget, budgets, setBudget})
     }
   }
 
+  // percentage of gift budget spent
+  // if nothing has been spent, return 0%
+  // else divide (amount spent)/(allocated budget) * 100 for percent
   const budgetPercentage = () => {
     if (budget) {
       if (parseInt(budget) === 0) {
